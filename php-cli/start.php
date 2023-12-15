8d55a397c054d2e454178eed512b0c6d0d478f66
Ignasiya <ignasiya93@gmail.com>
2023-12-15 20:44:30 +0300

    lesson-1 Readmy.md

--- php-cli/start.php
+++ php-cli/start.php
@@ -41,7 +41,4 @@
 
 // docker run --rm -v ${pwd}/php-cli/:/cli php:8.2-cli php /cli/start.php
 
-//docker run --rm -v ${pwd}/php-cli/:/cli php:7.4-cli php /cli/start.php
-
-?>;
-
+// docker run --rm -v ${pwd}/php-cli/:/cli php:7.4-cli php /cli/start.php
